86d28f9c8f28608a085794a83fd84979c0166683
jakubprzybylski15 <jakub.przybylski@zsbem.edu.pl>
2025-11-03 16:15:35 +0100

    Add files via upload

---
+++ wycieczki.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style4.css">
+    <title>Wycieczki po Europie</title>
+</head>
+<body>
+        <header> <h1>BIURO TURYSTYCZNE</h1> </header>
+
+    <nav>
+        <h3> Wycieszki, na które są wolne miejsca </h3> </nav>
+
+    <div id="col1"> <h2> Bestselery </h2>
+    </div>
+
+
+        <div id="col2"> <h2> Nasze zdjęcia </h2> </div>
+
+        <div id="col3"> <h2> Skontaktuj się </h2> <br>
+        napisz do nas <br><br>
+        telefon:111222333 </div>
+
+    <footer> strone wykonał:000 </footer>
+
+    
+</body>
+</html>
